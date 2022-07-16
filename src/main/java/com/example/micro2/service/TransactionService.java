0924f3ac096e55d4ab5df5fa400337402f07ef84
David Reyes <dareyes6@espe.edu.ec>
2022-07-15 22:43:27 -0500

    Implemented teacher's recommendations.

--- src/main/java/com/example/micro2/service/TransactionService.java
+++ src/main/java/com/example/micro2/service/TransactionService.java
@@ -1,10 +1,15 @@
 package com.example.micro2.service;
 
+import com.example.micro2.config.NarcClientConfig;
+import com.example.micro2.dto.NarcDTO;
 import com.example.micro2.enums.TransactionStateEnum;
+import com.example.micro2.exception.NotFoundException;
 import com.example.micro2.exception.TransactionException;
 import com.example.micro2.model.Account;
 import com.example.micro2.model.Client;
 import com.example.micro2.model.Transaction;
+import com.example.micro2.repository.AccountRepository;
+import com.example.micro2.repository.ClientRepository;
 import com.example.micro2.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,21 +23,20 @@ import java.util.UUID;
 public class TransactionService {
   private static final BigDecimal MIN_TRANSFER_CHECK_AMOUNT = BigDecimal.valueOf(1_000);
 
-  private final AccountService accountService;
-  private final ClientService clientService;
-  private final NarcClientService narcClientService;
-  private final TransactionRepository transactionRepository;
+  private final NarcClientConfig clientConfig;
+  private final AccountRepository accountRepository;
+  private final ClientRepository clientRepository;
+  final TransactionRepository transactionRepository;
 
   public Transaction makeTransaction(Transaction transaction) {
-    Account accountOrigin = accountService.findByInternalCode(transaction.getAccountOrigin());
-    Account accountDestination =
-        accountService.findByInternalCode(transaction.getAccountDestination());
+    Account accountOrigin = findAccountByInternalCode(transaction.getAccountOrigin());
+    Account accountDestination = findAccountByInternalCode(transaction.getAccountDestination());
 
-    Client clientDestination = clientService.findById(accountDestination.getClientId());
+    Client clientDestination = findClientById(accountDestination.getClientId());
 
-    boolean accountOriginHasEnougFunds =
+    boolean accountOriginHasEnoughFunds =
         accountOrigin.getBalance().compareTo(transaction.getAmount()) >= 0;
-    if (!accountOriginHasEnougFunds) {
+    if (!accountOriginHasEnoughFunds) {
       throw new TransactionException(
           "La cuenta " + accountOrigin.getInternalCode() + " no tiene fondos suficientes");
     }
@@ -40,8 +44,7 @@ public class TransactionService {
     boolean isAmountGreaterThanMinCheck =
         transaction.getAmount().compareTo(MIN_TRANSFER_CHECK_AMOUNT) > 0;
     if (isAmountGreaterThanMinCheck) {
-      boolean isClientNarc =
-          narcClientService.findNarcByName(clientDestination.getFullName()).getIsPenalized();
+      boolean isClientNarc = findNarcByName(clientDestination.getFullName()).getIsPenalized();
       if (isClientNarc) {
         transaction.setInternalCode(UUID.randomUUID().toString());
         transaction.setDate(new Date());
@@ -51,13 +54,56 @@ public class TransactionService {
       }
     }
 
-    accountService.withdrawAmountFromAccount(transaction.getAmount(), accountOrigin);
-    accountService.depositAmountIntoAccount(transaction.getAmount(), accountDestination);
+    withdrawAmountFromAccount(transaction.getAmount(), accountOrigin);
+    depositAmountIntoAccount(transaction.getAmount(), accountDestination);
 
     transaction.setInternalCode(UUID.randomUUID().toString());
     transaction.setDate(new Date());
     transaction.setState(TransactionStateEnum.EXECUTED.getValue());
 
     return transactionRepository.save(transaction);
+  }
+
+  public Account findAccountByInternalCode(Integer internalCode) {
+    return accountRepository
+        .findByInternalCode(internalCode)
+        .orElseThrow(
+            () ->
+                new NotFoundException("No se ha encontrado una cuenta con el id " + internalCode));
+  }
+
+  private Client findClientById(String clientId) {
+    return clientRepository
+        .findById(clientId)
+        .orElseThrow(
+            () -> new NotFoundException("No se ha encontrado un cliente con el id " + clientId));
+  }
+
+  public NarcDTO findNarcByName(String fullName) {
+    return clientConfig
+        .getClient()
+        .get()
+        .uri(uriBuilder -> uriBuilder.path("/search").queryParam("fullName", fullName).build())
+        .retrieve()
+        .bodyToMono(NarcDTO.class)
+        .block();
+  }
+
+  public void withdrawAmountFromAccount(BigDecimal amount, Account account) {
+    BigDecimal previousBalance = account.getBalance();
+    BigDecimal newBalance = previousBalance.subtract(amount);
+
+    account.setBalance(newBalance);
+
+    accountRepository.save(account);
+  }
+
+  public void depositAmountIntoAccount(BigDecimal amount, Account account) {
+    BigDecimal previousBalance = account.getBalance();
+    BigDecimal newBalance = previousBalance.add(amount);
+
+    account.setBalance(newBalance);
+
+    accountRepository.save(account);
   }
 }
